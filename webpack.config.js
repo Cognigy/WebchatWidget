1f4278d97c38a62f17124a719ee80bad86c3e0ab
Koenraad <k.hemelsoet@cognigy.com>
2018-07-04 18:28:55 +0200

    Integrate webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack.config.js
 // Configure CSS processing & injection
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
@@ -12,35 +13,40 @@ module.exports = {
 		library: 'Cognigy'
 	},
 	plugins: [
-		new webpack.optimize.CommonsChunkPlugin({
-			children: true, 
-			minChunks: 6
-			})
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			inject: 'head'
+		})
 	],
 	resolve: {
 		alias: {
 			"react": "preact-compat",
 			"react-dom": "preact-compat"
 		}
- 	},
+	},
 	module: {
 		rules: [
-				{
-					// Uses regex to test for a file type - in this case, ends with `.css`
-					test: /\.css$/,
-					// Apply these loaders if test returns true
-					use: ['style-loader/useable', 'css-loader']
-				},
-				{
-					test: /\.js|.jsx$/,
-					exclude: /(node_modules|bower_components)/,
-					use: {
-						loader: 'babel-loader',
-						options: {
+			{
+				// Uses regex to test for a file type - in this case, ends with `.css`
+				test: /\.css$/,
+				// Apply these loaders if test returns true
+				use: ['style-loader/useable', 'css-loader']
+			},
+			{
+				test: /\.js|.jsx$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
 						presets: ['@babel/preset-env']
 					}
 				}
 			}
 		]
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'build'),
+		compress: true,
+		publicPath: '/'
 	}
 }	
