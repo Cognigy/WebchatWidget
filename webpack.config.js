1e475cd5d4249525dc92f638c11fbe4e386d7bc2
Koenraad <k.hemelsoet@cognigy.com>
2018-07-05 16:54:49 +0200

    Add Flow  type checker, update Babel & webpack, use innerHtml for output

--- webpack.config.js
+++ webpack.config.js
@@ -36,10 +36,7 @@ module.exports = {
 				test: /\.js|.jsx$/,
 				exclude: /(node_modules|bower_components)/,
 				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
+					loader: 'babel-loader'
 				}
 			}
 		]
