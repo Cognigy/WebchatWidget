751e4ecad68eb225c0b9303397b8726b93654122
Cognigy Support <info@cognigy.com>
2019-06-06 11:27:30 +0200

    fix dependencies not transplied correctly for legacy builds

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,6 @@ module.exports = {
             {
                 // Include ts, tsx, js, and jsx files.
                 test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
                 loader: 'babel-loader',
                 options: {
                     presets: [
