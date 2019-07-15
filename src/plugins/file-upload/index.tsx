dae0178d8889dead03fce44bd80ee9be857e8552
Robin Schuster <r.schuster@cognigy.com>
2019-07-15 14:33:16 +0200

    upload to s3 plugin

--- src/plugins/file-upload/index.tsx
+++ src/plugins/file-upload/index.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import memoize from 'memoize-one';
 
 import { getStyles } from './styles';
@@ -8,106 +7,110 @@ import { useDropzone } from 'react-dropzone';
 // only re-calculate if theme changed
 const getStylesMemo = memoize(getStyles);
 
-const FileUpload = props => {
-    const onDrop = React.useCallback(async files => {
-        const file: File = files[0];
+const createFileUpload = React => {
+    const FileUpload = props => {
+        const onDrop = React.useCallback(async files => {
+            const file: File = files[0];
 
-        try {
-            const downloadUrl = await upload(props.message.data._plugin.payload, file);
-            props.onSendMessage('', {
-                file: downloadUrl
-            });
-        } catch (e) {
-            console.error('uploading file failed', e)
+            try {
+                const downloadUrl = await upload(props.message.data._plugin.payload, file);
+                props.onSendMessage('', {
+                    file: downloadUrl
+                });
+            } catch (e) {
+                console.error('uploading file failed', e)
+            }
+        }, []);
+        const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+        const {
+            isFullscreen,
+            onSetFullscreen,
+            theme,
+            message
+        } = props;
+
+        const {
+            openButtonLabel
+        } = message.data._plugin;
+
+        if (!isFullscreen) {
+            const { openDialogButtonStyles } = getStylesMemo(theme);
+
+            return (
+                <button
+                    type='button'
+                    onClick={onSetFullscreen}
+                    style={openDialogButtonStyles}
+                >
+                    {openButtonLabel || 'upload file'}
+                </button>
+            )
         }
-    }, []);
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
-    const {
-        isFullscreen,
-        onSetFullscreen,
-        theme,
-        message
-    } = props;
+        const {
+            attributes,
+            onDismissFullscreen,
+        } = props;
 
-    const {
-        openButtonLabel
-    } = message.data._plugin;
+        const {
+            payload,
+            titleText,
+            dragClickLabel,
+            dropLabel,
+            cancelButtonLabel
+        } = message.data._plugin;
 
-    if (!isFullscreen) {
-        const { openDialogButtonStyles } = getStylesMemo(theme);
+        const {
+            dialogStyles,
+            headerStyles,
+            contentStyles,
+            footerStyles,
+            cancelButtonStyles
+        } = getStylesMemo(theme);
 
         return (
-            <button
-                type='button'
-                onClick={onSetFullscreen}
-                style={openDialogButtonStyles}
+            <div
+                {...attributes}
+                style={{
+                    ...attributes.styles,
+                    ...dialogStyles
+                }}
             >
-                {openButtonLabel || 'upload file'}
-            </button>
+                {payload.service === 'aws-s3' && (
+                    <>
+                        <header style={headerStyles}>
+                            {titleText || 'File Upload'}
+                        </header>
+                        <main style={contentStyles} {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            {isDragActive
+                                ? <p>{dragClickLabel || 'Drop the file here'}</p>
+                                : <p>{dropLabel || 'Drag a file here, or click to select one'}</p>
+
+                            }
+                        </main>
+                        <footer style={footerStyles}>
+                            <button
+                                type='button'
+                                onClick={onDismissFullscreen}
+                                style={cancelButtonStyles}
+                            >
+                                {cancelButtonLabel || 'cancel'}
+                            </button>
+                        </footer>
+                    </>
+                )}
+            </div>
         )
     }
 
-    const {
-        attributes,
-        onDismissFullscreen,
-    } = props;
-
-    const { 
-        payload,
-        titleText,
-        dragClickLabel,
-        dropLabel,
-        cancelButtonLabel
-    } = message.data._plugin;
-
-    const {
-        dialogStyles,
-        headerStyles,
-        contentStyles,
-        footerStyles,
-        cancelButtonStyles
-    } = getStylesMemo(theme);
-
-    return (
-        <div
-            {...attributes}
-            style={{
-                ...attributes.styles,
-                ...dialogStyles
-            }}
-        >
-            {payload.service === 'aws-s3' && (
-                <>
-                    <header style={headerStyles}>
-                        {titleText || 'File Upload'}
-                    </header>
-                    <main style={contentStyles} {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        {isDragActive
-                            ? <p>{dragClickLabel || 'Drop the file here'}</p>
-                            : <p>{dropLabel || 'Drag a file here, or click to select one'}</p>
-
-                        }
-                    </main>
-                    <footer style={footerStyles}>
-                        <button
-                            type='button'
-                            onClick={onDismissFullscreen}
-                            style={cancelButtonStyles}
-                        >
-                            {cancelButtonLabel || 'cancel'}
-                    </button>
-                    </footer>
-                </>
-            )}
-        </div>
-    )
+    return FileUpload;
 }
 
 const fileUploadPlugin = ({ React }) => ({
     match: 'file-upload',
-    component: FileUpload
+    component: createFileUpload(React)
 })
 
 if (!window.cognigyWebchatMessagePlugins) {
