268ced301d3d9eb2a5baba52fc7a226ccb8469af
Robin Schuster <r.schuster@cognigy.com>
2019-07-25 12:09:31 +0200

    add version banner into bundles

--- webpack.production.js
+++ webpack.production.js
@@ -1,5 +1,14 @@
+const webpack = require('webpack');
+const { version } = require('./package.json');
+
 const config = require('./webpack.config');
 
 config.mode = 'production';
+config.plugins.push(
+    new webpack.BannerPlugin({
+        banner: `[file] v${version}\nhttps://github.com/Cognigy/WebchatWidget/tree/v${version}`
+    })
+)
+
 
 module.exports = config;
