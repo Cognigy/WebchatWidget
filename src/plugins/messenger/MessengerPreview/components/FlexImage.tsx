0bb8cc912f6ffa0d0c3233b846129d53da984317
Robin Schuster <r.schuster@cognigy.com>
2019-08-12 14:53:29 +0200

    WIP dynamic aspect images, hardcoded for now

---
+++ src/plugins/messenger/MessengerPreview/components/FlexImage.tsx
@@ -0,0 +1,10 @@
+import { MessagePluginFactoryProps } from "../../../../common/interfaces/message-plugin";
+
+export const getFlexImage = ({ React, styled }: MessagePluginFactoryProps) => {
+    const FlexImage = styled.img({
+        display: 'block',
+        width: '100%'
+    })
+
+    return FlexImage;
+}
