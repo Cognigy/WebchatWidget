0019a711eefd3d28fb6e14dd527d79811c241edb
Robin Schuster <r.schuster@cognigy.com>
2019-08-29 17:15:01 +0200

    switch to idempotent-babel-polyfill

--- webpack.production.legacy.js
+++ webpack.production.legacy.js
@@ -1,6 +1,6 @@
 const config = require('./webpack.production');
 
-config.entry.unshift('whatwg-fetch', '@babel/polyfill');
+config.entry.unshift('whatwg-fetch', 'idempotent-babel-polyfill');
 config.output.filename = 'webchat-legacy.bundle.js';
 config.module.rules[0].options.presets.unshift('@babel/preset-env');
 
