e98655e9f7858c53a6571d1a1dfd25c0e6a1371f
Cesar <cesar_cs_71@hotmail.com>
2019-12-02 18:45:35 +0100

    add classes to components

--- src/plugins/messenger/MessengerPreview/components/MessengerButtonTemplate/MessengerButtonTemplate.tsx
+++ src/plugins/messenger/MessengerPreview/components/MessengerButtonTemplate/MessengerButtonTemplate.tsx
@@ -1,47 +1,49 @@
-import { IFBMButtonTemplatePayload } from '../../interfaces/ButtonTemplatePayload.interface';
-import { IWithFBMActionEventHandler } from '../../MessengerPreview.interface';
-import { getDivider } from '../Divider';
-import { MessagePluginFactoryProps } from '../../../../../common/interfaces/message-plugin';
-import { getMessengerButton } from '../MessengerButton/MessengerButton';
-import { getMessengerFrame } from '../MessengerFrame';
+import { IFBMButtonTemplatePayload } from "../../interfaces/ButtonTemplatePayload.interface";
+import { IWithFBMActionEventHandler } from "../../MessengerPreview.interface";
+import { getDivider } from "../Divider";
+import { MessagePluginFactoryProps } from "../../../../../common/interfaces/message-plugin";
+import { getMessengerButton } from "../MessengerButton/MessengerButton";
+import { getMessengerFrame } from "../MessengerFrame";
 
 interface IMessengerButtonTemplateProps extends IWithFBMActionEventHandler {
-    payload: IFBMButtonTemplatePayload
+  payload: IFBMButtonTemplatePayload;
 }
 
-export const getMessengerButtonTemplate = ({ React, styled }: MessagePluginFactoryProps) => {
-    const MessengerFrame = getMessengerFrame({ React, styled });
-    const MessengerButton = getMessengerButton({ React, styled });
-    const Divider = getDivider({ React, styled });
+export const getMessengerButtonTemplate = ({
+  React,
+  styled
+}: MessagePluginFactoryProps) => {
+  const MessengerFrame = getMessengerFrame({ React, styled });
+  const MessengerButton = getMessengerButton({ React, styled });
+  const Divider = getDivider({ React, styled });
 
-    const Text = styled.div(({ theme }) => ({
-        padding: theme.unitSize
-    }));
+  const Text = styled.div(({ theme }) => ({
+    padding: theme.unitSize
+  }));
 
-    const MessengerButtonTemplate = ({ payload, onAction, ...divProps }: IMessengerButtonTemplateProps & React.HTMLProps<HTMLDivElement>) => {
-        const { text, buttons } = payload;
+  const MessengerButtonTemplate = ({
+    payload,
+    onAction,
+    ...divProps
+  }: IMessengerButtonTemplateProps & React.HTMLProps<HTMLDivElement>) => {
+    const { text, buttons } = payload;
 
-        return (
-            <MessengerFrame {...divProps}>
-                {text && (
-                    <Text>
-                        {text}
-                    </Text>
-                )}
-                {buttons.map((button, index) => (
-                    <React.Fragment
-                        key={index}
-                    >
-                        <Divider />
-                        <MessengerButton
-                            button={button}
-                            onClick={e => onAction(e, button)}
-                        />
-                    </React.Fragment>
-                ))}
-            </MessengerFrame>
-        )
-    }
+    return (
+      <MessengerFrame {...divProps} className="messenger-buttons-header">
+        {text && <Text>{text}</Text>}
+        {buttons.map((button, index) => (
+          <React.Fragment key={index}>
+            <Divider />
+            <MessengerButton
+              button={button}
+              onClick={e => onAction(e, button)}
+              className="messenger-button"
+            />
+          </React.Fragment>
+        ))}
+      </MessengerFrame>
+    );
+  };
 
-    return MessengerButtonTemplate;
-}
+  return MessengerButtonTemplate;
+};
--- src/plugins/messenger/MessengerPreview/components/MessengerGenericTemplate/MessengerGenericTemplate.tsx
+++ src/plugins/messenger/MessengerPreview/components/MessengerGenericTemplate/MessengerGenericTemplate.tsx
@@ -1,135 +1,153 @@
-import { IFBMGenericTemplatePayload, IFBMGenericTemplateElement } from '../../interfaces/GenericTemplatePayload.interface';
-import { IWithFBMActionEventHandler } from '../../MessengerPreview.interface';
-import { getDivider } from '../Divider';
-import { MessagePluginFactoryProps } from '../../../../../common/interfaces/message-plugin';
-import { getMessengerButton } from '../MessengerButton/MessengerButton';
-import { getMessengerContent } from '../MessengerContent';
-import { getMessengerFrame } from '../MessengerFrame';
-import { getMessengerTitle } from '../MessengerTitle';
-import { getMessengerSubtitle } from '../MessengerSubtitle';
-import { Carousel } from 'react-responsive-carousel';
+import {
+  IFBMGenericTemplatePayload,
+  IFBMGenericTemplateElement
+} from "../../interfaces/GenericTemplatePayload.interface";
+import { IWithFBMActionEventHandler } from "../../MessengerPreview.interface";
+import { getDivider } from "../Divider";
+import { MessagePluginFactoryProps } from "../../../../../common/interfaces/message-plugin";
+import { getMessengerButton } from "../MessengerButton/MessengerButton";
+import { getMessengerContent } from "../MessengerContent";
+import { getMessengerFrame } from "../MessengerFrame";
+import { getMessengerTitle } from "../MessengerTitle";
+import { getMessengerSubtitle } from "../MessengerSubtitle";
+import { Carousel } from "react-responsive-carousel";
 
-import './carousel.css';
-import { IWebchatConfig } from '@cognigy/webchat-client/lib/interfaces/webchat-config';
-import { getFlexImage } from '../FlexImage';
-import { getBackgroundImage } from '../../lib/css';
+import "./carousel.css";
+import { IWebchatConfig } from "@cognigy/webchat-client/lib/interfaces/webchat-config";
+import { getFlexImage } from "../FlexImage";
+import { getBackgroundImage } from "../../lib/css";
 
-export interface IMessengerGenericTemplateProps extends IWithFBMActionEventHandler {
-    payload: IFBMGenericTemplatePayload;
-    config: IWebchatConfig;
+export interface IMessengerGenericTemplateProps
+  extends IWithFBMActionEventHandler {
+  payload: IFBMGenericTemplatePayload;
+  config: IWebchatConfig;
 }
 
 export interface IMessengerGenericTemplateState {
-    index: number;
+  index: number;
 }
 
-export const getMessengerGenericTemplate = ({ React, styled }: MessagePluginFactoryProps) => {
-    const MessengerSubtitle = getMessengerSubtitle({ React, styled });
-    const MessengerTitle = getMessengerTitle({ React, styled });
-    const MessengerFrame = getMessengerFrame({ React, styled });
-    const MessengerContent = getMessengerContent({ React, styled });
-    const MessengerButton = getMessengerButton({ React, styled });
-    const Divider = getDivider({ React, styled });
-    const FlexImage = getFlexImage({ React, styled });
+export const getMessengerGenericTemplate = ({
+  React,
+  styled
+}: MessagePluginFactoryProps) => {
+  const MessengerSubtitle = getMessengerSubtitle({ React, styled });
+  const MessengerTitle = getMessengerTitle({ React, styled });
+  const MessengerFrame = getMessengerFrame({ React, styled });
+  const MessengerContent = getMessengerContent({ React, styled });
+  const MessengerButton = getMessengerButton({ React, styled });
+  const Divider = getDivider({ React, styled });
+  const FlexImage = getFlexImage({ React, styled });
 
-    const CarouselRoot = styled(Carousel)(({ theme }) => ({
-        marginBottom: -32,
+  const CarouselRoot = styled(Carousel)(({ theme }) => ({
+    marginBottom: -32,
 
-        '.slide': {
-            paddingLeft: theme.unitSize * 2,
-            paddingRight: theme.unitSize * 2,
-            paddingBottom: 32,
-            display: 'flex',
+    ".slide": {
+      paddingLeft: theme.unitSize * 2,
+      paddingRight: theme.unitSize * 2,
+      paddingBottom: 32,
+      display: "flex",
 
-            '&>*': {
-                flexGrow: 1
-            }
-        }
-    }))
-
-    const ElementRoot = styled.div(({ theme }) => ({
-        display: 'flex',
-    }));
-
-    const Frame = styled(MessengerFrame)({
-        backgroundColor: 'white',
-        display: 'flex',
-        flexDirection: 'column',
-
-        '&.wide': {
-            width: 320
-        }
-    });
-
-    const FixedImage = styled.div({
-        paddingTop: '50%',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center center'
-    });
-
-    const GenericContent = styled(MessengerContent)({
+      "&>*": {
         flexGrow: 1
-    });
-
-    const MessengerGenericTemplate = class MessengerGenericTemplate extends React.Component<IMessengerGenericTemplateProps & React.HTMLProps<HTMLDivElement>, IMessengerGenericTemplateState> {
-        renderElement = (element: IFBMGenericTemplateElement, index?: number) => {
-
-            const { onAction, ...divProps } = this.props;
-            const { image_url, title, subtitle, buttons, default_action } = element;
-
-            const isCentered = this.props.config.settings.designTemplate === 2;
-
-            const image = image_url
-                ? this.props.config.settings.dynamicImageAspectRatio
-                    ? <FlexImage  style={{ backgroundImage: getBackgroundImage(image_url) }} />
-                    : <FixedImage style={{ backgroundImage: getBackgroundImage(image_url) }} />
-                : null
-
-            return (
-                <ElementRoot key={index}>
-                    <Frame className={isCentered ? 'wide' : ''}>
-                        {image}
-                        <GenericContent
-                            onClick={e => default_action && onAction(e, default_action)}
-                        >
-                            <MessengerTitle>{title}</MessengerTitle>
-                            <MessengerSubtitle>{subtitle}</MessengerSubtitle>
-                        </GenericContent>
-                        {buttons && buttons.map((button, index) => (
-                            <React.Fragment key={index}>
-                                <Divider />
-                                <MessengerButton
-                                    button={button}
-                                    onClick={e => onAction(e, button)}
-                                />
-                            </React.Fragment>
-                        ))}
-                    </Frame>
-                </ElementRoot>
-            );
-        }
-
-        render() {
-            const elements = this.props.payload.elements;
-
-            if (elements.length === 0)
-                return null;
-
-            if (elements.length === 1)
-                return this.renderElement(elements[0])
-
-            return (
-                <CarouselRoot
-                    showThumbs={false}
-                    showIndicators={false}
-                    showStatus={false}
-                    centerMode={true}
-                >
-                    {elements.map(this.renderElement)}
-                </CarouselRoot>
-            )
-        }
+      }
     }
+  }));
 
-    return MessengerGenericTemplate;
-}
+  const ElementRoot = styled.div(({ theme }) => ({
+    display: "flex"
+  }));
+
+  const Frame = styled(MessengerFrame)({
+    backgroundColor: "white",
+    display: "flex",
+    flexDirection: "column",
+
+    "&.wide": {
+      width: 320
+    }
+  });
+
+  const FixedImage = styled.div({
+    paddingTop: "50%",
+    backgroundSize: "cover",
+    backgroundPosition: "center center"
+  });
+
+  const GenericContent = styled(MessengerContent)({
+    flexGrow: 1
+  });
+
+  const MessengerGenericTemplate = class MessengerGenericTemplate extends React.Component<
+    IMessengerGenericTemplateProps & React.HTMLProps<HTMLDivElement>,
+    IMessengerGenericTemplateState
+  > {
+    renderElement = (element: IFBMGenericTemplateElement, index?: number) => {
+      const { onAction, ...divProps } = this.props;
+      const { image_url, title, subtitle, buttons, default_action } = element;
+
+      const isCentered = this.props.config.settings.designTemplate === 2;
+
+      const image = image_url ? (
+        this.props.config.settings.dynamicImageAspectRatio ? (
+          <FlexImage
+            style={{ backgroundImage: getBackgroundImage(image_url) }}
+          />
+        ) : (
+          <FixedImage
+            style={{ backgroundImage: getBackgroundImage(image_url) }}
+          />
+        )
+      ) : null;
+
+      return (
+        <ElementRoot key={index} className="messenger-carousel-container">
+          <Frame className={isCentered ? "wide" : ""}>
+            {image}
+            <GenericContent
+              onClick={e => default_action && onAction(e, default_action)}
+            >
+              <MessengerTitle className="messenger-carousel-title">
+                {title}
+              </MessengerTitle>
+              <MessengerSubtitle className="messenger-carousel-subtitle">
+                {subtitle}
+              </MessengerSubtitle>
+            </GenericContent>
+            {buttons &&
+              buttons.map((button, index) => (
+                <React.Fragment key={index}>
+                  <Divider />
+                  <MessengerButton
+                    button={button}
+                    onClick={e => onAction(e, button)}
+                  />
+                </React.Fragment>
+              ))}
+          </Frame>
+        </ElementRoot>
+      );
+    };
+
+    render() {
+      const elements = this.props.payload.elements;
+
+      if (elements.length === 0) return null;
+
+      if (elements.length === 1) return this.renderElement(elements[0]);
+
+      return (
+        <CarouselRoot
+          showThumbs={false}
+          showIndicators={false}
+          showStatus={false}
+          centerMode={true}
+        >
+          {elements.map(this.renderElement)}
+        </CarouselRoot>
+      );
+    }
+  };
+
+  return MessengerGenericTemplate;
+};
