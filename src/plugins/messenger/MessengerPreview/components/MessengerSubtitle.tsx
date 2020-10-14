71130d15589b9a38d8cd2a147aa5b4d0fe0ce8ae
Cesar Guillen <c.guillen@cognigy.com>
2020-10-14 11:14:59 +0200

    change to pre-line

--- src/plugins/messenger/MessengerPreview/components/MessengerSubtitle.tsx
+++ src/plugins/messenger/MessengerPreview/components/MessengerSubtitle.tsx
@@ -6,7 +6,7 @@ export const getMessengerSubtitle = ({ React, styled }: MessagePluginFactoryProp
         margin: 0,
         color: 'hsla(0, 0%, 0%, .54)',
         fontSize: 13,
-        whiteSpace: props.config.settings.ignoreLineBreaks ? "initial" : "pre-wrap" ,
+        whiteSpace: props.config.settings.ignoreLineBreaks ? "initial" : "pre-line" ,
     }));
 
     return MessengerSubtitle;
