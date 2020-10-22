ce70dd6d7dbf7b15673abea69ad91b31ed65a4b9
David Scheier <d.scheier@cognigy.com>
2020-10-22 10:59:57 +0200

    prevent resetState to overwrite connection state with storage state

--- src/webchat/store/reducer.ts
+++ src/webchat/store/reducer.ts
@@ -23,7 +23,12 @@ export const resetState = (state?: StoreState) => ({
 export const reducer = (state = rootReducer(undefined, { type: '' }), action) => {
     switch (action.type) {
         case 'RESET_STATE': {
-            return rootReducer(action.state, { type: '' })
+
+			// We do not want to restore some properties from storage but keep the value from state
+			const newState = action.state as StoreState;
+			newState.connection.connected = state.connection.connected;
+
+			return rootReducer(newState, { type: '' })
         }
     }
 
