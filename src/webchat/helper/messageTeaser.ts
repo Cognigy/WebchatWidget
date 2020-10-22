2f7152ae33a1881b8168c223dc3dc8e2444fc13d
Alexander Teusz <teusz.alexander@gmail.com>
2020-10-22 15:15:58 +0200

    handle empty text in mesageTeaser helper

--- src/webchat/helper/messageTeaser.ts
+++ src/webchat/helper/messageTeaser.ts
@@ -3,12 +3,14 @@ import { IMessage } from "../../common/interfaces/message"
 const getTextFromMessage = (message: IMessage) => {
 
     // Check if message is plain text
-    if (message.text !== null) {
+    if (message.text) {
         return message.text;
     
         // Check if message is quick reply message
     } else if (message?.data?._cognigy?._webchat?.message?.text) {
         return message.data._cognigy._webchat.message.text;
+    } else {
+        return "";
     }
 }
 
