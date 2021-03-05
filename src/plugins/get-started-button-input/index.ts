48d35cde3a86204697b4928d817bee412f1acc8a
Robin Schuster <r.schuster@cognigy.com>
2021-03-05 18:14:37 +0100

    make exception for an "engagement mesage" in the "get started button" rule

--- src/plugins/get-started-button-input/index.ts
+++ src/plugins/get-started-button-input/index.ts
@@ -3,7 +3,7 @@ import { InputRule, InputPlugin } from "../../common/interfaces/input-plugin";
 import { registerInputPlugin } from "../helper";
 
 const rule: InputRule = ({ config: { settings: { startBehavior, getStartedButtonText, getStartedPayload, getStartedText } }, messages }) =>
-    messages.length === 0
+    (messages.length === 0 || messages.length === 1 && messages[0].source === 'engagement')
     && startBehavior === 'button'
     && !!getStartedPayload
     && (!!getStartedButtonText || !!getStartedText)
