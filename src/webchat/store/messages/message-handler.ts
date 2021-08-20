8e93f46cd632f40cbf17ff27185a322fc563cb90
David Scheier <d.scheier@cognigy.com>
2021-08-20 14:01:59 +0200

    trigger rating on request plugin msg

--- src/webchat/store/messages/message-handler.ts
+++ src/webchat/store/messages/message-handler.ts
@@ -3,6 +3,7 @@ import { IMessage } from "../../../common/interfaces/message";
 import { ISendMessageOptions } from "./message-middleware";
 import { setBotAvatarOverrideUrl, setUserAvatarOverrideUrl, setAgentAvatarOverrideUrl, setTyping } from "../ui/ui-reducer";
 import { SocketClient } from "@cognigy/socket-client";
+import { showRatingDialog } from "../rating/rating-reducer";
 
 const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
 export const receiveMessage = (message: IMessage, options: Partial<ISendMessageOptions> = {}) => ({
@@ -30,6 +31,15 @@ export const registerMessageHandler = (store: Store, client: SocketClient) => {
                 store.dispatch(setUserAvatarOverrideUrl(userAvatarOverrideUrl))
             }
         }
+
+		// handle custom plugin actions
+		if (output.data && output.data._plugin) {
+			const { type } = output.data._plugin;
+
+			if (type === "request-rating") {
+				store.dispatch(showRatingDialog(true));
+			};
+		}
 
         store.dispatch(setTyping("remove"));
         store.dispatch(receiveMessage(output));
