079fc82811a4d7daffa57246915742fd5f3774ae
Robin Schuster <r.schuster@cognigy.com>
2021-09-15 17:09:30 +0200

    improve message renderer tests

--- cypress/integration/message-renderer/availability.spec.ts
+++ cypress/integration/message-renderer/availability.spec.ts
@@ -7,19 +7,30 @@ describe("Generic Message Renderer", () => {
       expect(window?.MessageRenderer?.renderMessage).to.be.a("function");
     });
   });
-  it("renders a text to a dom node", () => {
+  it("renders a text message to a dom node", () => {
+    cy.window()
+      .then((window) => {
+        const root = window.document.createElement("div");
+        root.innerText = "before";
+
+        window.document.body.appendChild(root);
+
+        return root;
+      })
+      .as("root");
+
+    cy.contains("before");
+
     cy.window().then((window) => {
-      const root = window.document.createElement("div");
-      root.innerText = "before";
-
-      window.document.body.appendChild(root);
-
-      window.MessageRenderer.renderMessage(
-        { text: "hello world", data: {} },
-        root
-      );
+      cy.get("@root").then((root) => {
+        window.MessageRenderer.renderMessage(
+          { text: "hello world", data: {} },
+          root.get()[0]
+        );
+      });
     });
 
     cy.contains("hello world");
+    cy.contains("before").should("not.exist");
   });
 });
