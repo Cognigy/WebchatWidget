2aa8cc4c8589bb36831d625e6656e30a5bd89955
David Scheier <d.scheier@cognigy.com>
2021-09-25 09:31:27 +0200

    fix test

--- selenium-test.js
+++ selenium-test.js
@@ -44,13 +44,10 @@ async function runTestWithCaps() {
 		const nextChatMessages = await driver.findElements(webdriver.By.className("webchat-message-row"));
 		const sentMessageIndex = nextChatMessages.findIndex(elem => elem.innerText === "Browser Test");
 
-		// We check if we found the sent message
 		if (sentMessageIndex === -1) throw new Error("Send Message failed")
 
-		// We check if the sent message is last or if we have a response
 		if (sentMessageIndex === nextChatMesages.length - 1) throw new Error("Receive Message failed")
 
-		// if (chatMessageSum + 2 !== nextChatMessages.length) throw new Error("send & receive Message failed.")
 
 		await driver.executeScript(
 			'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Received response!"}}'
