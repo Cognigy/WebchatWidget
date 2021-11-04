9bcbddae4639a2eb287ee8b2b39e76651aa4d79f
Robin Schuster <r.schuster@cognigy.com>
2021-11-04 10:51:59 +0100

    serve both, "webchat" and "message renderer", when running "npm run dev"

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,14 @@
 const config = require('./webpack.config');
+const path = require('path');
+
+config.entry = {
+    'webchat': './src/webchat-embed/index.tsx',
+    'message-renderer': './src/message-renderer/embed.ts'
+};
+
+config.output = {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+};
 
 module.exports = config;
