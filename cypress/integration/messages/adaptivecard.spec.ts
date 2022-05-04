e107b8f546c0b4ef0920927d53ea301954142288
Robin Schuster <r.schuster@cognigy.com>
2022-05-04 09:12:37 +0200

    fix tests

--- cypress/integration/messages/adaptivecard.spec.ts
+++ cypress/integration/messages/adaptivecard.spec.ts
@@ -34,6 +34,32 @@ describe("Message with AdaptiveCard", () => {
                 });
             });
         });
+
+        it("renders a hyperlink when rendering markdown markup", () => {
+            cy.withMessageFixture('adaptivecard-markdown', () => {
+                cy.contains("Here's some text with markdown syntax and a hyperlink embedded right into it.").should("be.visible");
+    
+                cy.contains("strong", "markdown").should('be.visible');
+    
+                cy.contains("a", "hyperlink").then((anchor) => {
+                    expect(anchor.attr('href')).to.equal("https://example.org");
+                });
+            });
+        });   
+        
+        it("navigates when clicking an Action.OpenUrl button", () => {
+            cy.withMessageFixture('adaptivecard-actions', () => {
+                cy.window().then(window => {
+                    cy.stub(window, 'open').as("winOpen");
+                });
+                
+                cy.contains("Open Example Page").click();
+    
+                cy.get("@winOpen").then(open => {
+                    expect(open).to.be.calledOnceWith("https://example.org", "_blank");
+                })
+            });
+        });
     });
 
     it("prefers the sideoaded adaptivecards plugin over the internal one", () => {
@@ -57,31 +83,5 @@ describe("Message with AdaptiveCard", () => {
 
         cy.contains("Your registration is almost complete").should("be.visible");
         cy.get(".adaptivecard-wrapper.internal").should("be.visible");
-    });
-
-    it("renders a hyperlink when rendering markdown markup", () => {
-        cy.withMessageFixture('adaptivecard-markdown', () => {
-            cy.contains("Here's some text with markdown syntax and a hyperlink embedded right into it.").should("be.visible");
-
-            cy.contains("strong", "markdown").should('be.visible');
-
-            cy.contains("a", "hyperlink").then((anchor) => {
-                expect(anchor.attr('href')).to.equal("https://example.org");
-            });
-        });
-    });   
-    
-    it("navigates when clicking an Action.OpenUrl button", () => {
-        cy.withMessageFixture('adaptivecard-actions', () => {
-            cy.window().then(window => {
-                cy.stub(window, 'open').as("winOpen");
-            });
-            
-            cy.contains("Open Example Page").click();
-
-            cy.get("@winOpen").then(open => {
-                expect(open).to.be.calledOnceWith("https://example.org", "_blank");
-            })
-        });
     });
 });
