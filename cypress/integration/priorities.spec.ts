aa01295ebcc131021355d8da38f9ab2b375284e1
Robin Schuster <r.schuster@cognigy.com>
2022-08-09 18:03:57 +0200

    added automated tests for "default preview" feature

--- cypress/integration/priorities.spec.ts
+++ cypress/integration/priorities.spec.ts
@@ -1,15 +1,17 @@
 
 interface IGenerateTestCaseParams {
-    webchatTabConfigured: boolean;
-    facebookTabConfigured: boolean;
-    syncWebchatWithFacebookConfigured: boolean;
+    defaultPreviewTabConfigured?: boolean;
+    webchatTabConfigured?: boolean;
+    facebookTabConfigured?: boolean;
+    syncWebchatWithFacebookConfigured?: boolean;
     enableStrictMessengerSync: boolean;
-    expectedOutcome: 'webchat' | 'facebook' | 'none';
+    enableDefaultPreview?: boolean;
+    expectedOutcome: 'default' | 'webchat' | 'facebook' | 'none' | null;
 }
 
 const renderMessageWithParams = (params: IGenerateTestCaseParams) => {
     cy.visitMessageRenderer();
-    
+
     const config = {
         settings: {
             enableStrictMessengerSync: params.enableStrictMessengerSync
@@ -25,6 +27,7 @@ const renderMessageWithParams = (params: IGenerateTestCaseParams) => {
 
     const messageData = {
         _cognigy: {
+            _defaultPreview: params.defaultPreviewTabConfigured ? getDummyMessage('RENDER DEFAULTPREVIEW') : null,
             _webchat: params.webchatTabConfigured ? getDummyMessage('RENDER WEBCHAT') : null,
             _facebook: params.facebookTabConfigured ? getDummyMessage("RENDER FACEBOOK") : null,
             syncWebchatWithFacebook: params.syncWebchatWithFacebookConfigured
@@ -43,17 +46,50 @@ const expectOutcomeFromParams = (params: IGenerateTestCaseParams) => {
         case 'webchat':
             return cy.contains('RENDER WEBCHAT').should('be.visible');
 
+        case 'default':
+            return cy.contains('RENDER DEFAULTPREVIEW').should('be.visible');
+
         case 'none':
-            return cy.contains('RENDER').should('not.be.visible');
+        case null:
+            return cy.contains('RENDER').should('not.exist');
     }
 }
 
 const getTestDescriptionFromParams = (params: IGenerateTestCaseParams) => {
-    return `If _webchat: ${params.webchatTabConfigured},
-    _facebook: ${params.facebookTabConfigured}, 
-    enableStrictMessengerSync: ${params.enableStrictMessengerSync},
-    syncWebchatWithFacebookConfigured: ${params.syncWebchatWithFacebookConfigured}
-    Should render ${params.expectedOutcome}`; // generated test tescription from params
+    const expectedResult = (() => {
+        if (params.expectedOutcome === 'none' || params.expectedOutcome === null)
+            return 'should not render anything';
+
+        return `expecting a "${params.expectedOutcome}" message.`;
+    })();
+
+    const messageProperties = (() => {
+        const tabNames = [
+            params.defaultPreviewTabConfigured && 'default',
+            params.webchatTabConfigured && 'webchat',
+            params.facebookTabConfigured && 'facebook',
+            params.syncWebchatWithFacebookConfigured && 'syncWebchatWithFacebook'
+        ].filter(a => !!a);
+
+        return `message has ${tabNames.join(', ')}.`
+    })();
+
+    const flags = (() => {
+        const flagNames = [
+            params.enableStrictMessengerSync && 'enableStrictMessengerSync',
+            params.enableDefaultPreview && 'enableDefaultPreview',
+        ].filter(a => !!a);
+
+        if (!flagNames)
+            return 'no flags are set.';
+
+        if (flagNames.length === 1)
+            return `flag ${flagNames[0]} is set.`;
+
+        return `flags ${flagNames.join(', ')} are set.`;
+    })();
+
+    return `${messageProperties} ${flags} ${expectedResult}`;
 }
 
 const generateTestCase = (params: IGenerateTestCaseParams) => {
@@ -68,47 +104,169 @@ const generateTestCase = (params: IGenerateTestCaseParams) => {
     });
 }
 
-type GenerateTestCaseParamsArray = [boolean, boolean, boolean, boolean, IGenerateTestCaseParams['expectedOutcome']];
-
-const getTestCaseObject = ([webchatTabConfigured, facebookTabConfigured, syncWebchatWithFacebookConfigured, enableStrictMessengerSync, expectedOutcome]: GenerateTestCaseParamsArray): IGenerateTestCaseParams => ({
-    webchatTabConfigured,
-    facebookTabConfigured,
-    syncWebchatWithFacebookConfigured,
-    enableStrictMessengerSync,
-    expectedOutcome
-});
-
-describe("Channel Rendering Priorities", () => {
+describe("Channel Rendering Priorities", {
+    defaultCommandTimeout: 500
+}, () => {
     describe("Messenger Plugin", () => {
+        type GenerateTestCaseParamsArray = [boolean, boolean, boolean, boolean, IGenerateTestCaseParams['expectedOutcome']];
+
+        const getTestCaseObject = ([webchatTabConfigured, facebookTabConfigured, syncWebchatWithFacebookConfigured, enableStrictMessengerSync, expectedOutcome]: GenerateTestCaseParamsArray): IGenerateTestCaseParams => ({
+            webchatTabConfigured,
+            facebookTabConfigured,
+            syncWebchatWithFacebookConfigured,
+            enableStrictMessengerSync,
+            expectedOutcome
+        });
+
         describe("Without strict Messenger Sync", () => {
             const testcases = [
                 // [_webchat, _facebook, syncWebchatWithFacebook, enableStrictMessengerSync, expectedResult]
-                [   false,    false,     false,                   false,                     null          ],
-                [   true,     false,     false,                   false,                     'webchat'     ],
-                [   false,    true,      false,                   false,                     'facebook'    ],
-                [   true,     true,      false,                   false,                     'webchat'     ],
+                [false, false, false, false, null],
+                [true, false, false, false, 'webchat'],
+                [false, true, false, false, 'facebook'],
+                [true, true, false, false, 'webchat'],
             ];
-            
+
             testcases.map(getTestCaseObject).forEach(generateTestCase);
         });
 
         describe("With strict Messenger Sync", () => {
             const testcases = [
                 // [_webchat, _facebook, syncWebchatWithFacebook, enableStrictMessengerSync, expectedResult]
-                [   true,     false,     false,                   true,                      'webchat'     ],
-                [   false,    true,      false,                   true,                      null          ],
-                [   true,     true,      false,                   true,                      'webchat'     ],
-                [   false,    false,     false,                   true,                      null          ],
-                [   false,    false,     true,                    true,                      null          ],
-                [   true,     false,     true,                    true,                      null          ],
-                [   false,    true,      true,                    true,                      'facebook'    ],
-                [   true,     true,      true,                    true,                      'facebook'    ],
+                [true, false, false, true, 'webchat'],
+                [false, true, false, true, null],
+                [true, true, false, true, 'webchat'],
+                [false, false, false, true, null],
+                [false, false, true, true, null],
+                [true, false, true, true, null],
+                [false, true, true, true, 'facebook'],
+                [true, true, true, true, 'facebook'],
             ];
-            
+
             testcases.map(getTestCaseObject).forEach(generateTestCase);
         });
     });
 
+    describe("Default Previews", () => {
+        describe('_defaultPreview is always preferred over _webchat and _facebook if enableDefaultPreview is set', () => {
+            // _defaultPreview, _webchat, _facebook, enableDefaultPreview, expectedResult
+            const cases = [
+                [0, 0, 0, 0, 'none'],
+                [1, 0, 0, 0, 'none'],
+                [0, 1, 0, 0, 'webchat'],
+                [1, 1, 0, 0, 'webchat'],
+                [0, 0, 1, 0, 'facebook'],
+                [1, 0, 1, 0, 'facebook'],
+                [0, 1, 1, 0, 'webchat'],
+                [1, 1, 1, 0, 'webchat'],
+                [0, 0, 0, 1, 'none'],
+                [1, 0, 0, 1, 'default'],
+                [0, 1, 0, 1, 'webchat'],
+                [1, 1, 0, 1, 'default'],
+                [0, 0, 1, 1, 'facebook'],
+                [1, 0, 1, 1, 'default'],
+                [0, 1, 1, 1, 'webchat'],
+                [1, 1, 1, 1, 'default'],
+            ];
+
+            cases
+                .map(([
+                    _defaultPreview,
+                    _webchat,
+                    _facebook,
+                    enableDefaultPreview,
+                    expectedOutcome
+                ]): IGenerateTestCaseParams => ({
+                    enableStrictMessengerSync: false,
+                    expectedOutcome: expectedOutcome as any,
+                    defaultPreviewTabConfigured: !!_defaultPreview,
+                    facebookTabConfigured: !!_facebook,
+                    webchatTabConfigured: !!_webchat,
+                    enableDefaultPreview: !!enableDefaultPreview,
+                    syncWebchatWithFacebookConfigured: false
+                }))
+                .forEach(generateTestCase);
+        });
+
+
+        describe('_defaultPreview is preferred for all builtin message plugins if enableDefaultPreview is set', () => {
+            it('should prefer a text from the default tab over quick replies from the webchat tab', () => {
+                cy.visitMessageRenderer();
+
+                cy.renderMessage("DEFAULT TEXT", {
+                    _cognigy: {
+                        _webchat: {
+                            message: {
+                                text: "WEBCHAT QUICK REPLIES"
+                            }
+                        }
+                    }
+                }, "bot", {
+                    enableDefaultPreview: true
+                });
+
+                cy.contains("DEFAULT TEXT").should('be.visible');
+            });
+
+            it('should prefer quick replies from the default tab over quick replies from the webchat tab', () => {
+                cy.visitMessageRenderer();
+
+                cy.renderMessage("", {
+                    _cognigy: {
+                        _default: {
+                            message: {
+                                text: "DEFAULT QUICK REPLIES"
+                            }
+                        },
+                        _webchat: {
+                            message: {
+                                text: "WEBCHAT QUICK REPLIES"
+                            }
+                        },
+                    }
+                }, "bot", {
+                    enableDefaultPreview: true
+                });
+
+                cy.contains("DEFAULT QUICK REPLIES").should('be.visible');
+            });
+
+            it('should prefer adaptivecards from the default tab over quick replies from the webchat tab', () => {
+                cy.visitMessageRenderer();
+
+                cy.renderMessage("", {
+                    _cognigy: {
+                        _default: {
+                            _adaptiveCard: {
+                                type: "adaptiveCard",
+                                adaptiveCard: {
+                                    type: "AdaptiveCard",
+                                    $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
+                                    version: "1.5",
+                                    body: [
+                                        {
+                                            type: "TextBlock",
+                                            text: "DEFAULT ADAPTIVECARDS",
+                                            wrap: true
+                                        }
+                                    ]
+                                }
+                            }
+                        },
+                        _webchat: {
+                            message: {
+                                text: "WEBCHAT QUICK REPLIES"
+                            }
+                        }
+                    }
+                }, "bot", {
+                    enableDefaultPreview: true
+                });
+
+                cy.contains("DEFAULT ADAPTIVECARDS").should('be.visible');
+            });
+        });
+    });
 });
 
 
