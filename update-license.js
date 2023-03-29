2f4d5b7efa4a75fb4c52e8972b450fdb7d7b7894
Sushmitha Sekar <sushmithasekar28@gmail.com>
2023-03-29 18:55:16 +0200

    Change indentation to spaces

--- update-license.js
+++ update-license.js
@@ -4,32 +4,32 @@ const result = new Map();
 let processing = 0;
 
 checker.init({
-	start: "./",
-	development: false,
-	production: true,
+    start: "./",
+    development: false,
+    production: true,
 }, function (err, packages) {
-	processing++;
-	if (err) {
-		processing--;
-	} else {
-		const keys = Object.keys(packages);
-		for (let i = 0, il = keys.length; i < il; i++) {
-			if (packages[keys[i]].publisher === "Cognigy GmbH") {
-				continue;
-			}
-			let name = keys[i].split("@");
-			name.pop();
-			name = name.join("@");
-			const data = {
-				license: packages[keys[i]].licenses,
-				websiteUrl: packages[keys[i]].url || packages[keys[i]].repository,
-				authors: packages[keys[i]].publisher,
-			}
-			result.set(name, data);
-		}
-		processing--;
-		if (processing === 0) {
-			console.dir(Array.from(result, ([name, value]) => ({ name, ...value })), { 'maxArrayLength': null });
-		}
-	}
+    processing++;
+    if (err) {
+        processing--;
+    } else {
+        const keys = Object.keys(packages);
+        for (let i = 0, il = keys.length; i < il; i++) {
+            if (packages[keys[i]].publisher === "Cognigy GmbH") {
+                continue;
+            }
+            let name = keys[i].split("@");
+            name.pop();
+            name = name.join("@");
+            const data = {
+                license: packages[keys[i]].licenses,
+                websiteUrl: packages[keys[i]].url || packages[keys[i]].repository,
+                authors: packages[keys[i]].publisher,
+            }
+            result.set(name, data);
+        }
+        processing--;
+        if (processing === 0) {
+            console.dir(Array.from(result, ([name, value]) => ({ name, ...value })), { 'maxArrayLength': null });
+        }
+    }
 });
