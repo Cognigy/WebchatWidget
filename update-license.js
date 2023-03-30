ae36956c4180bc233c29b56fcae6d46cf4395795
Sushmitha Sekar <sushmithasekar28@gmail.com>
2023-03-30 11:43:22 +0200

    Fix indentation

--- update-license.js
+++ update-license.js
@@ -2,26 +2,25 @@ const checker = require('license-checker');
 const fs = require('fs');
 
 checker.init({
-  start: "./",
-  development: false,
-  production: true,
+    start: "./",
+    development: false,
+    production: true,
 }, function (err, packages) {
     if (err) {
-      console.log(err);
+        console.log(err);
     } else {
-			const updatedLicenseText = `${getDateString()}\n\n${JSON.stringify(packages, null, 2)}`
-			fs.writeFileSync("./OSS_LICENSES.txt", updatedLicenseText);
+        const updatedLicenseText = `${getDateString()}\n\n${JSON.stringify(packages, null, 2)}`;
+        fs.writeFileSync("./OSS_LICENSES.txt", updatedLicenseText);
     }
-	}
-);
+});
 
 function getDateString() {
-	const date = new Date();
-	const year = date.getFullYear();
-	const month = `${date.getMonth() + 1}`.padStart(2, '0');
-	const day =`${date.getDate()}`.padStart(2, '0');
-	const hours = `${date.getHours()}`.padStart(2, '0');
-	const minutes = `${date.getMinutes()}`.padStart(2, '0');
-	const seconds = `${date.getSeconds()}`.padStart(2, '0');
-	return `Created on ${day}-${month}-${year} at ${hours}:${minutes}:${seconds}`;
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const day =`${date.getDate()}`.padStart(2, '0');
+    const hours = `${date.getHours()}`.padStart(2, '0');
+    const minutes = `${date.getMinutes()}`.padStart(2, '0');
+    const seconds = `${date.getSeconds()}`.padStart(2, '0');
+    return `Created on ${day}-${month}-${year} at ${hours}:${minutes}:${seconds}`;
 }
