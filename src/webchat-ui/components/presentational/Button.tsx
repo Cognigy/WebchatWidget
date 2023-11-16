f63889f6cb8760e307432d84b8faca9fe87d044a
David Scheier <d.scheier@cognigy.com>
2023-11-16 12:23:01 +0100

    adjust font size according to specs

--- src/webchat-ui/components/presentational/Button.tsx
+++ src/webchat-ui/components/presentational/Button.tsx
@@ -5,61 +5,64 @@ import { interactionCss, createTransition } from "../../utils/css";
 
 export default styled.button<IColorProps>(({ color, theme }) => {
 
-    const colors = {
-        weak: theme.greyWeakColor,
-        main: theme.greyColor,
-        strong: theme.greyStrongColor,
-        contrast: theme.greyContrastColor
-    }
+	const colors = {
+		weak: theme.greyWeakColor,
+		main: theme.greyColor,
+		strong: theme.greyStrongColor,
+		contrast: theme.greyContrastColor
+	}
 
-    if (color === 'primary') {
-        colors.weak = theme.secondaryColorDisabled;
-        colors.main = theme.secondaryColor;
-        colors.strong = theme.secondaryColorHover;
-        colors.contrast = theme.secondaryContrastColor;
-    }
+	if (color === 'primary') {
+		colors.weak = theme.secondaryColorDisabled;
+		colors.main = theme.secondaryColor;
+		colors.strong = theme.secondaryColorHover;
+		colors.contrast = theme.secondaryContrastColor;
+	}
 
-    return {
-        ...interactionCss,
+	return {
+		...interactionCss,
 
-        borderRadius: 10,
-        display: "flex",
-        width: 303,
-        height: 44,
-        padding: "11px 0px",
-        justifyContent: "center",
-        alignItems: "center",
-        flexShrink: 0,
+		borderRadius: 10,
+		display: "flex",
+		width: 303,
+		height: 44,
+		padding: "11px 0px",
+		justifyContent: "center",
+		alignItems: "center",
+		flexShrink: 0,
 
-        backgroundColor: colors.main,
-        color: colors.contrast,
+		backgroundColor: colors.main,
+		color: colors.contrast,
 
-        textTransform: 'unset',
-        fontWeight: 'bold',
+		textTransform: 'unset',
+		fontSize: 14,
+		fontStyle: "normal",
+		fontWeight: 600,
+		lineHeight: "160%",
 
-        border: 'none',
-        outline: 'none',
+		border: 'none',
+		outline: 'none',
 
-        cursor: 'pointer',
+		cursor: 'pointer',
 
-        transition: createTransition('background-color'),
+		transition: createTransition('background-color'),
 
-        '&:disabled': {
-            opacity: .4,
-            cursor: 'default',
-            backgroundColor: colors.weak,
-        },
+		'&:disabled': {
+			opacity: .4,
+			cursor: 'default',
+			backgroundColor: colors.weak,
+		},
 
-        '&:hover:not(:disabled)': {
-            backgroundColor: colors.strong,
-        },
+		'&:hover:not(:disabled)': {
+			backgroundColor: colors.strong,
+		},
 
-        '&:focus:not(:disabled)': {
-            backgroundColor: colors.strong,
-        },
+		'&:focus:not(:disabled)': {
+			backgroundColor: colors.strong,
+		},
 
-        '&:active:not(:disabled)': {
-            backgroundColor: colors.weak,
-        }
-    }
+		'&:active:not(:disabled)': {
+			backgroundColor: colors.weak,
+		}
+	}
 });
