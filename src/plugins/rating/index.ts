8897617ff8b15a26747e37223178138fd47fec27
Sushmitha Sekar <sushmithasekar28@gmail.com>
2024-01-03 17:28:47 +0100

    Remove Rating Dialog and unused files

--- src/plugins/rating/index.ts
+++ src/plugins/rating/index.ts
@@ -1,38 +1,13 @@
 import { registerMessagePlugin } from "../helper";
 import { MessagePlugin } from "../../common/interfaces/message-plugin";
-import RatingHistoryEntry from "./RatingHistoryEntry";
-import { IMessage } from "../../common/interfaces/message";
 
-interface IRating {
-  rating: number;
-  comment?: string;
-}
-
-export const getRating = (message: IMessage): IRating | null => {
-  const { source, data } = message;
-
-  if (
-    source === "user" &&
-    data?._cognigy?.controlCommands?.[0]?.type === "setRating"
-  ) {
-    const { comment, rating } =
-      message?.data?._cognigy?.controlCommands?.[0]?.parameters;
-
-    return { comment, rating };
-  }
-
-  if ((message as any)?.rating !== undefined) {
-    const { rating, ratingComment: comment } = message as any;
-
-    return { rating, comment };
-  }
-
+export const getRating = () => {
   return null;
 };
 
 const ratingMessagePlugin: MessagePlugin = {
-  match: (message) => !!getRating(message),
-  component: RatingHistoryEntry,
+  match: () => !!getRating(),
+  component: () => null,
   options: {
     fullwidth: true,
   },
