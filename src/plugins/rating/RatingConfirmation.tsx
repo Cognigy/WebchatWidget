a4f5d7ced5aaddd6e96521d84ce1f56757df61cf
Sushmitha Sekar <sushmithasekar28@gmail.com>
2024-01-03 12:42:42 +0100

    Add rating confirmation text

---
+++ src/plugins/rating/RatingConfirmation.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ChatEvent } from "@cognigy/chat-components";
+
+const RatingConfirmation = () => {
+	return <ChatEvent text="Feedback submitted" />;
+};
+
+export default RatingConfirmation;
