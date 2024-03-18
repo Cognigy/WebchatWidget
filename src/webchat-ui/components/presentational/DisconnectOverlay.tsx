409e863ed504fdcb1cc65d2456fb045934ea3566
Dmitrii Ostasevich <90881+kwinto@users.noreply.github.com>
2024-03-18 12:11:16 +0100

    restyle and fix appearance on secondary screens

--- src/webchat-ui/components/presentational/DisconnectOverlay.tsx
+++ src/webchat-ui/components/presentational/DisconnectOverlay.tsx
@@ -1,78 +1,83 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import IconButton from './IconButton';
-import CloseIcon from '../../assets/baseline-close-24px.svg';
-import Button from './Button';
+import React from "react";
+import styled from "@emotion/styled";
+import IconButton from "./IconButton";
+import CloseIcon from "../../assets/baseline-close-24px.svg";
+import Button from "./Button";
 
 const Wrapper = styled.div(({ theme }) => ({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
 
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    width: '100%',
-    height: '100%',
+	position: "absolute",
+	left: 0,
+	top: 0,
+	width: "100%",
+	height: "100%",
 
-    padding: theme.blockSize,
-    boxSizing: 'border-box',
-    backgroundColor: 'hsla(0, 0%, 0%, .33)',
+	padding: 20,
+	boxSizing: "border-box",
+	backgroundColor: "theme.primaryColor",
+	backdropFilter: "blur(18px)",
 
-    zIndex: 2,
+	zIndex: 4,
 }));
 
 const Dialog = styled.div(({ theme }) => ({
-    padding: theme.unitSize * 2,
-    borderRadius: theme.unitSize,
+	padding: theme.unitSize * 2,
+	borderRadius: theme.unitSize,
 
-    backgroundColor: theme.backgroundWebchat,
-    color: theme.greyContrastColor,
-
-    boxShadow: theme.shadow,
-    textAlign: "center",
+	textAlign: "center",
 }));
 
-const DialogHeader = styled.div(({theme}) => ({
-    marginBottom: theme.unitSize * 2
-}))
+const DialogHeader = styled.div(({ theme }) => ({
+	marginBottom: theme.unitSize * 2,
+	fontSize: 22,
+	fontWeight: 600,
+}));
 
 const HeaderIconButton = styled(IconButton)(({ theme }) => ({
-    position: 'absolute',
-    right: theme.unitSize,
-    top: theme.unitSize + 1,
+	position: "absolute",
+	right: 16,
+	top: 16,
 
-    color: theme.primaryContrastColor,
-    fill: theme.primaryContrastColor,
-    borderRadius: "50%",
-    "&:focus, &:hover": {
-        backgroundColor: theme.primaryStrongColor,
-        fill: `${theme.primaryContrastColor} !important`,
-        opacity: 0.85,
-    }
+	color: theme.textDark,
+	fill: theme.textDark,
+	borderRadius: "50%",
 }));
 
 const DisconnectOverlay = ({ isPermanent, onClose, onConnect }) => {
-    return (
-        <Wrapper>
-            <Dialog>
-                {isPermanent ? (
-                    <><DialogHeader>Connection lost</DialogHeader>
-                    {navigator.onLine ? <Button onClick={onConnect} color="primary">Reconnect</Button> : <div>No network connection</div>}</>
-                ) : (
-                        <span>Connection lost. Trying to reconnect...</span>
-                    )}
-            </Dialog>
-            <HeaderIconButton
-                data-disconnect-overlay-close-button
-                onClick={onClose}
-                className="webchat-header-close-button"
-                aria-label="Close Warning"
-            >
-                <CloseIcon />
-            </HeaderIconButton>
-        </Wrapper>
-    );
+	return (
+		<Wrapper>
+			<Dialog>
+				{isPermanent ? (
+					<>
+						<DialogHeader>Connection lost</DialogHeader>
+						{navigator.onLine ? (
+							<Button onClick={onConnect} color="primary">
+								Reconnect
+							</Button>
+						) : (
+							<div>No network connection</div>
+						)}
+					</>
+				) : (
+					<div>
+						<DialogHeader>Connection lost</DialogHeader>
+						<div>Reconnecting...</div>
+					</div>
+				)}
+			</Dialog>
+			<HeaderIconButton
+				data-disconnect-overlay-close-button
+				onClick={onClose}
+				className="webchat-header-close-button"
+				aria-label="Close Warning"
+			>
+				<CloseIcon />
+			</HeaderIconButton>
+		</Wrapper>
+	);
 };
 
-export default DisconnectOverlay
+export default DisconnectOverlay;
