106fc48368e201eb1552bb4727e3e2dd4bcab551
a.ionova <a.ionova@cognigy.com>
2026-01-14 16:26:00 +0100

    add highlighting for button with options

--- src/plugins/messenger/MessengerPreview/components/MessengerButton/MessengerButton.tsx
+++ src/plugins/messenger/MessengerPreview/components/MessengerButton/MessengerButton.tsx
@@ -1,76 +1,108 @@
-
-import { IFBMButton } from '../../interfaces/Button.interface';
-import { getButtonLabel } from './lib/messengerButtonHelpers';
-import { MessagePluginFactoryProps } from '../../../../../common/interfaces/message-plugin';
-import { IWebchatConfig } from '../../../../../common/interfaces/webchat-config';
-import { sanitizeHTML } from '../../../../../webchat/helper/sanitize';
+import { IFBMButton } from "../../interfaces/Button.interface";
+import { getButtonLabel } from "./lib/messengerButtonHelpers";
+import { MessagePluginFactoryProps } from "../../../../../common/interfaces/message-plugin";
+import { IWebchatConfig } from "../../../../../common/interfaces/webchat-config";
+import { sanitizeHTML } from "../../../../../webchat/helper/sanitize";
 
 interface IMessengerButtonProps {
-    button: IFBMButton;
-    config: IWebchatConfig;
-    position?: number;
-    total?: number
+	button: IFBMButton;
+	config: IWebchatConfig;
+	position?: number;
+	total?: number;
+	selectedOptionIndex?: number;
+	buttonIndex?: number;
 }
 
 export const getMessengerButton = ({ React, styled }: MessagePluginFactoryProps) => {
+	const componentStyles = ({ theme }) => ({
+		display: "block",
+		color: theme.primaryColor,
+		border: "none",
+		backgroundColor: "white",
+		padding: `10px 20px`,
+		fontSize: 15,
+		cursor: "pointer",
+		outline: "none",
 
-    const componentStyles = (({ theme }) => ({
-        display: 'block',
-        color: theme.primaryColor,
-        border: 'none',
-        backgroundColor: 'white',
-        padding: `10px 20px`,
-        fontSize: 15,
-        cursor: 'pointer',
-        outline: 'none',
+		"&:hover": {
+			backgroundColor: "hsl(0, 0%, 97%)",
+		},
 
-        '&:hover': {
-            backgroundColor: 'hsl(0, 0%, 97%)'
-        },
-        
-        '&:focus': {
-            backgroundColor: 'hsl(0, 0%, 92%)'
-        },
+		"&:focus": {
+			backgroundColor: "hsl(0, 0%, 92%)",
+		},
 
-        '&:active': {
-            backgroundColor: 'hsl(0, 0%, 92%)'
-        }
-    }))
+		"&:active": {
+			backgroundColor: "hsl(0, 0%, 92%)",
+		},
+	});
 
-    const Button = styled.button(componentStyles, {
-        width: '100%',
-    });
+	const Button = styled.button<{ isSelected?: boolean }>(({ theme, isSelected }) => {
+		return {
+			...componentStyles({ theme }),
+			width: "100%",
+			...(isSelected && {
+				backgroundColor: "hsl(0, 0%, 92%)",
+			}),
+		};
+	});
 
-    const Link = styled.a(componentStyles, {
-        textDecoration: 'none',
-        textAlign: 'center'
-    });
+	const Link = styled.a<{ isSelected?: boolean }>(({ theme, isSelected }) => {
+		return {
+			...componentStyles({ theme }),
+			textDecoration: "none",
+			textAlign: "center",
+			...(isSelected && {
+				backgroundColor: "hsl(0, 0%, 92%)",
+			}),
+		};
+	});
 
-    const MessengerButton = ({ button, position, total, ...props }: IMessengerButtonProps & React.ComponentProps<typeof Button>) => {		
-        const buttonType = button.type;
-        const buttonPayload = button.payload;
-        const isWebURL = buttonType === "web_url";
-        const isPhoneNumber = buttonPayload && buttonType === "phone_number";
-        const buttonTitle = button.title ? button.title + ". " : "";
-        const isWebURLButtonTargetBlank = button.target !== "_self";
-        const buttonTitleWithTarget = isWebURL && isWebURLButtonTargetBlank ? buttonTitle + "Opens in new tab" : button.title;
-        const ariaLabel = total > 1 ? `Item ${position} of ${total}: ${buttonTitleWithTarget}` : buttonTitleWithTarget;
-    
-        const buttonLabel = getButtonLabel(button);
-        const __html = props.config.settings.disableHtmlContentSanitization ? buttonLabel : sanitizeHTML(buttonLabel)
+	const MessengerButton = ({
+		button,
+		position,
+		total,
+		selectedOptionIndex,
+		buttonIndex,
+		...props
+	}: IMessengerButtonProps & React.ComponentProps<typeof Button>) => {
+		const buttonType = button.type;
+		const buttonPayload = button.payload;
+		const isWebURL = buttonType === "web_url";
+		const isPhoneNumber = buttonPayload && buttonType === "phone_number";
+		const buttonTitle = button.title ? button.title + ". " : "";
+		const isWebURLButtonTargetBlank = button.target !== "_self";
+		const buttonTitleWithTarget =
+			isWebURL && isWebURLButtonTargetBlank ? buttonTitle + "Opens in new tab" : button.title;
+		const ariaLabel =
+			total > 1
+				? `Item ${position} of ${total}: ${buttonTitleWithTarget}`
+				: buttonTitleWithTarget;
 
-        const Component = isPhoneNumber ? Link : Button;
+		const buttonLabel = getButtonLabel(button);
+		const __html = props.config.settings.disableHtmlContentSanitization
+			? buttonLabel
+			: sanitizeHTML(buttonLabel);
 
-        return (
-            <Component
-                {...props} 
-                dangerouslySetInnerHTML={{__html}} 
-                role={isWebURL ? "link" : undefined} 
-                aria-label={ariaLabel}
-                href={isPhoneNumber ? `tel:${buttonPayload}` : undefined}
-            />
-        )
-    }
+		// Check if this button is selected
+		const isSelected =
+			selectedOptionIndex !== undefined &&
+			buttonIndex !== undefined &&
+			selectedOptionIndex === buttonIndex;
 
-    return MessengerButton;
-}
+		const Component = isPhoneNumber ? Link : Button;
+
+		return (
+			<Component
+				{...props}
+				dangerouslySetInnerHTML={{ __html }}
+				role={isWebURL ? "link" : undefined}
+				aria-label={ariaLabel}
+				href={isPhoneNumber ? `tel:${buttonPayload}` : undefined}
+				isSelected={isSelected}
+			/>
+		);
+	};
+
+	return MessengerButton;
+};
